1b1436c05db704369bd0b620566c68d67d0abf51
kevin <kevincolmenaresm@gmail.com>
2023-09-12 00:02:34 -0400

    page and layout modificated

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import Content from "./Content";
+
+export const metadata = {
+  title: "Home",
+  description: "Home page",
+};
+
+export default function Home() {
+
+  return (
+    <div>
+      <Content />
+    </div>
+  );
+}
