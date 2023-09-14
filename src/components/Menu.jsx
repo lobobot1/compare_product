60d6b0dac8ba46903d840f241466cf95894bfd92
kevin <kevincolmenaresm@gmail.com>
2023-09-13 20:39:06 -0400

    add menu sidebar

---
+++ src/components/Menu.jsx
@@ -0,0 +1,39 @@
+'use client'
+import useFetching from "../hooks/useFetching";
+
+export const product_type = [
+    "nike",
+    "lollipop",
+    "singer",
+    "maison",
+    "giotto",
+    "ljacquard"
+  ];
+
+const Menu = () => {
+
+  const {setOrigin, setSimilar, origin} = useFetching()
+
+  const handleClick = (type) =>{
+    setOrigin(`${type.toLowerCase()}o`)
+    setSimilar(`${type.toLowerCase()}s`)
+  }
+
+  return (
+    <div>
+        <ul className="flex flex-col items-center text-left gap-3 pt-12 px-5">
+            {product_type.map((product) => (
+              <li key={product} className={`w-full text-2xl ${product===origin.slice(0,-1) ? 'text-[#2ed89a]' : 'text-[#cfd1d5]'}  font-semibold`}>
+                  <button 
+                    className="capitalize"
+                    onClick={()=> handleClick(product)}>
+                    {product}
+                  </button>
+              </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default Menu
