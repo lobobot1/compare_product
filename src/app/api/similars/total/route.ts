b111ebdac683328cbcbb9bd321ca8ba7398cc055
kevin <kevincolmenaresm@gmail.com>
2023-09-26 17:49:41 -0400

    adjust and add routes

---
+++ src/app/api/similars/total/route.ts
@@ -0,0 +1,20 @@
+import { prisma } from "../../../../lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET(req: NextRequest): Promise<NextResponse> {
+    const similar = req.nextUrl.searchParams.get("similar");
+
+    if(!similar) return NextResponse.json({ error: "No similar provided" }, { status: 400 });
+
+    const total = await prisma[similar].count();
+
+    return NextResponse.json({
+        data: total,
+        message: "Total found",
+        ok: true,
+    },{
+        status: 200,
+        statusText: "OK",
+    });
+}
