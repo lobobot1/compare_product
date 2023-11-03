6022505e4c9f41d118eedf6c35b6d28ade558d40
kevin <kevincolmenaresm@gmail.com>
2023-11-03 10:48:37 -0400

    [update] prisma asignation

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,3 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+const globalForPrisma = globalThis;
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
